d38e3bfde0a541d405a98bbdd41645092e701de4
aminami1127 <aminami1127@users.noreply.github.com>
2016-04-10 08:34:13 +0900

    mod

--- cgi-bin/bot.py
+++ cgi-bin/bot.py
@@ -44,7 +44,7 @@ def handle_endtag(self, tag):
 def receive():
     data = json.loads(sys.stdin.read())
     result = data['result'][0]
-    return result['from'], result['content']['text'].split('　')
+    return result['from'], result['content']['text'].split(',')
 
 
 def reply(user, result):
