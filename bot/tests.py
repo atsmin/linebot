793142d87d07932c21fb972988416f2d68fc5af9
aminami1127 <aminami1127@users.noreply.github.com>
2016-05-24 21:48:17 +0900

    mod tests.py for trivas-ci

--- bot/tests.py
+++ bot/tests.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 import unittest
 from datetime import datetime
+import pytz
 
 from api import make_message
 
@@ -10,7 +11,9 @@ class LastTrainMessageTest(unittest.TestCase):
     def test_normal(self):
         """正しい入力値の場合は正常に終電時刻を取得できること"""
         text = '上野から鶯谷'
-        result = make_message(text, now=datetime(2016, 5, 24, 20, 0))
+        result = make_message(
+            text, now=datetime(2016, 5, 24, 20, 0, tzinfo=pytz.timezone('Asia/Tokyo'))
+        )
         assert '上野→鶯谷' in result
         assert '経路1' in result
 
@@ -49,7 +52,9 @@ class LastTrainMessageTest(unittest.TestCase):
     def test_already_left1(self):
         """既に終電がないときは始発の時間を調べて返すこと(1)"""
         text = '横浜から大宮'
-        result = make_message(text, now=datetime(2016, 5, 24, 23, 30))
+        result = make_message(
+            text, now=datetime(2016, 5, 24, 23, 30, tzinfo=pytz.timezone('Asia/Tokyo'))
+        )
         assert '横浜→大宮' in result
         assert '経路1' in result
         assert '始発の時間' in result
@@ -57,7 +62,9 @@ class LastTrainMessageTest(unittest.TestCase):
     def test_already_left2(self):
         """既に終電がないときは始発の時間を調べて返すこと(2)"""
         text = '渋谷から鶯谷'
-        result = make_message(text, now=datetime(2016, 5, 25, 0, 30))
+        result = make_message(
+            text, now=datetime(2016, 5, 25, 0, 30, tzinfo=pytz.timezone('Asia/Tokyo'))
+        )
         assert '渋谷→鶯谷' in result
         assert '経路1' in result
         assert '始発の時間' in result
